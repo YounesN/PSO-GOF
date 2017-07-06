38aed281631e7eb5c20c5037477b937c79e13e90
Younes Nejahi <younes@wayne.edu>
2017-07-06 19:28:16 -0400

    change

--- water/main.cpp
+++ water/main.cpp
@@ -2,6 +2,15 @@
 #include "info.h"
 #include "restriction.h"
 
+double current_charge;
+double current_epsilon;
+double current_sigma;
+int current_n;
+double current_bond;
+
+double current_temperature;
+int iteration = 0;
+
 int main()
 {
   PrintRestrictions();
