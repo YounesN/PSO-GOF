9b335dc3294d6ba74b4973724ffe7ea5778b3275
Younes Nejahi <younes@wayne.edu>
2017-11-17 12:22:43 -0500

    fixed n

---
+++ fixed_n/younes.cmd
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --time=900:00:00
+#SBATCH --ntasks=2   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --exclude=potoff[16,40,43]
+echo Running on host `hostname`
+echo Time is `date`
+
+cd /home4/younes/Desktop/Optimization/AV_ARG/
+echo Directory is `pwd`
+
+# Run job
+./main
