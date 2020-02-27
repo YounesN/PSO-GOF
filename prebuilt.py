ec41b7f55416cbcedfd69c76dd3acdb9d202d801
Younes Nejahi <younes@wayne.edu>
2020-02-26 16:06:34 -0800

    bug fix

--- prebuilt.py
+++ prebuilt.py
@@ -15,12 +15,6 @@ size = comm.Get_size()
 rank = comm.Get_rank()
 
 sys.path.append("./include/prebuilt/")
-from parameter import Parameter, Parameters
-from temperature import Temperature, Temperatures
-from particleswarm import ParticleSwarmParameters
-from utility import Utility
-from particle import Particle
-from simulation import Simulation
 from pso import PSO
 
 pso = PSO(30, 80, 'par.xml')
